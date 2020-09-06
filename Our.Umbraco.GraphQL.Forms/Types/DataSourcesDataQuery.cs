6165ea4825443b02b0c437b12cb5c12ddb48bbea
Benjamin Carleski <benjamin@proworks.com>
2020-09-05 20:59:15 -0700

    Switch all to use Id

--- Our.Umbraco.GraphQL.Forms/Types/DataSourcesDataQuery.cs
+++ Our.Umbraco.GraphQL.Forms/Types/DataSourcesDataQuery.cs
@@ -1,4 +1,5 @@
 using Our.Umbraco.GraphQL.Attributes;
+using Our.Umbraco.GraphQL.Types;
 using System;
 using System.Collections.Generic;
 using Umbraco.Forms.Core;
@@ -10,6 +11,6 @@ namespace Our.Umbraco.GraphQL.Forms.Types
     {
         public IEnumerable<FormDataSource> All([Inject] IDataSourceStorage dataSourceStorage) => dataSourceStorage.GetAllDataSources();
 
-        public FormDataSource ById([Inject] IDataSourceStorage dataSourceStorage, string id) => Guid.TryParse(id, out var guid) ? dataSourceStorage.GetDataSource(guid) : null;
+        public FormDataSource ById([Inject] IDataSourceStorage dataSourceStorage, Id id) => Guid.TryParse(id.Value, out var guid) ? dataSourceStorage.GetDataSource(guid) : null;
     }
 }
--- Our.Umbraco.GraphQL.Forms/Types/PreValueSourcesDataQuery.cs
+++ Our.Umbraco.GraphQL.Forms/Types/PreValueSourcesDataQuery.cs
@@ -1,4 +1,5 @@
 using Our.Umbraco.GraphQL.Attributes;
+using Our.Umbraco.GraphQL.Types;
 using System;
 using System.Collections.Generic;
 using Umbraco.Forms.Core.Interfaces;
@@ -10,6 +11,6 @@ namespace Our.Umbraco.GraphQL.Forms.Types
     {
         public IEnumerable<IFieldPreValueSource> All([Inject] IPrevalueSourceStorage prevalueSourceStorage) => prevalueSourceStorage.GetAllPrevalueSources();
 
-        public IFieldPreValueSource ById([Inject] IPrevalueSourceStorage prevalueSourceStorage, string id) => Guid.TryParse(id, out var guid) ? prevalueSourceStorage.GetPrevalueSource(guid) : null;
+        public IFieldPreValueSource ById([Inject] IPrevalueSourceStorage prevalueSourceStorage, Id id) => Guid.TryParse(id.Value, out var guid) ? prevalueSourceStorage.GetPrevalueSource(guid) : null;
     }
 }
